c5919201ee948350472ff8a248c16f02485ecb28
Omar <omar@omar-System-Product-Name.(none)>
2012-08-27 15:52:52 -0300

    When creating tickets, attribute them to the creator.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,8 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
-  attr_accessible :description, :title
+  belongs_to :user
+  attr_accessible :description, :title, :user
+  #agregue :user porque sino me tiraba Can't mass-assign protected attributes: user
   validates :title, :presence => true
   validates :description, :presence => true
   validates :description, :presence => true,
