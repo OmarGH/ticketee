162729d1736660c98a61d5053ba28332d0097757
Omar <omar@omar-System-Product-Name.(none)>
2012-09-07 23:45:43 -0300

    Added permissions screen for admins

---
+++ app/controllers/admin/permissions_controller.rb
@@ -0,0 +1,13 @@
+class Admin::PermissionsController < Admin::BaseController
+  before_filter :find_user
+  
+  def index
+    @ability = Ability.new(@user)
+    @projects = Project.all
+  end
+  
+  private
+    def find_user
+      @user = User.find(params[:user_id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Ticketee::Application.routes.draw do
     :to => "users#confirmation",
     :as => 'confirm_user'
   
+  put '/admin/users/:user_id/permissions',
+    :to => 'admin/permissions#update',
+    :as => :update_user_permissions
+
+  
   root :to => "projects#index"
   resources :projects do
     resources :tickets
@@ -13,9 +18,12 @@ Ticketee::Application.routes.draw do
   
   namespace :admin do    
     root :to => "base#index"
-    resources :users
+    resources :users do
+      resources :permissions
+    end
   end
 
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
